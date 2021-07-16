23cd27bf6d828ba1f4db4aaaa7d09b4f2f926356
Nathan Lucy <cosmocoder1@gmail.com>
2021-07-16 10:57:50 -0500

    first three functions complete - isEven in progress

--- src/recursion.js
+++ src/recursion.js
@@ -7,20 +7,47 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null
+  }
+
+  if (n === 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var accumulator = 0;
+
+    if (!Array.isArray(array)) {
+       return array;
+    }
+
+    for (var i = 0; i < array.length; i++) {
+    accumulator += arraySum(array[i]);
+    };
+
+  return accumulator;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+
+
 };
 
 // 5. Sum all integers below a given integer.
