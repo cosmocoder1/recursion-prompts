72d908528e5e3e354e84605b5c1a1a3e99496471
Nathan Lucy <cosmocoder1@gmail.com>
2021-07-20 14:36:43 -0500

    countValuesInObj in progress

--- src/recursion.js
+++ src/recursion.js
@@ -409,6 +409,24 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+
+  var count = 0;
+
+    if (Object(obj) !== obj) {
+      return obj;
+    }
+
+    for (key in obj) {
+      var current = obj[key];
+      if (Object(current) !== current) {
+        if (value === current) {
+          count += 1;
+        } else {
+        countValuesInObj(current, value);
+      }
+    return count;
+    }
+  }
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
