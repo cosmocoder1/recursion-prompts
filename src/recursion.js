989d037b22a59dfd55fc550305d1b58cebbab739
Nathan Lucy <cosmocoder1@gmail.com>
2021-07-20 12:41:52 -0500

    gcd complete

--- src/recursion.js
+++ src/recursion.js
@@ -323,7 +323,22 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+
+  if (x < 0 || y < 0) {
+    return null;
+  }
+
+  if (y === 0) {
+    return x;
+  }
+
+  modulo = x % y;
+  x = y;
+  y = modulo;
+  return gcd(x, y)
+
 };
+
 
 // 15. Write a function that compares each character of two strings and returns true if
 // both are identical.
